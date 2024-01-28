bd144565d502d8798b3e763c288bc11bf606ac34
david383812@gmail.com <david383812@gmail.com>
2024-01-28 09:11:58 -0500

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,25 @@
+export default function CustomersPage(){
+    return (
+        <>
+            <section className="flex flex-row flex-wrap">
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-cyan-500">1</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-red-500">2</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+                <div className="border-black border-2 opacity-90 w-[100px] h-[100px] bg-yellow-500">3</div>
+            </section>
+        </> 
+    )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+          {children}
+          <footer className='py-10 flex justify-center items-center'>
+            Hecho con ❤ por la gente de Vercel
+          </footer>
+      </body>
+    </html>
+  );
+}
